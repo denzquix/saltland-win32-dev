51bc107a72bea534cfc0c61babfaa9bd8c05d4f9
Duncan Cross <duncan.cross@gmail.com>
2013-02-10 15:40:13 +0000

    Add -e parameter to set up package paths

--- launch-win32/launch-win32.c
+++ launch-win32/launch-win32.c
@@ -22,6 +22,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpCmdLine, i
 	return 0;
   }
   
+  wchar_t exe_path[MAX_PATH];
+  GetCurrentDirectoryW(MAX_PATH, exe_path);
+  
   wchar_t project_path[MAX_PATH] = L"";
   
   OPENFILENAMEW ofn;
@@ -39,10 +42,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpCmdLine, i
   }
   
   wchar_t project_folder_path[MAX_PATH];
+  memset(project_folder_path, 0, sizeof(project_folder_path));
   wcsncpy(project_folder_path, project_path, ofn.nFileOffset);
   
-  wchar_t cmd_line[1 + luajit_path_len + 3 + launch_path_len + 3 + wcslen(project_path) + 1 + 1];
-  wsprintfW(cmd_line, L"\"%s\" \"%s\" \"%s\"", luajit_path, launch_path, project_path);
+  wchar_t setup[MAX_PATH*6 + 1024] = L"-e \"local m={X=[[";
+  wcscat(setup, exe_path);
+  wcscat(setup, L"]],P=[[");
+  wcsncat(setup, project_path, ofn.nFileOffset-1);
+  wcscat(setup, L"]]} package.path=('?.lua;?/init.lua;$P/?.lua;$P/?/init.lua;$X/lua/?.lua;$X/lua/?/init.lua'):gsub('$(.)',m) package.cpath=('?.dll;$P/win32-bin/?.dll;$X/win32-bin/?.dll'):gsub('$(.)',m)\"");
+  
+  wchar_t cmd_line[1 + luajit_path_len + 2 + wcslen(setup) + 2 + launch_path_len + 3 + wcslen(project_path) + 1 + 1];
+  wsprintfW(cmd_line, L"\"%s\" %s \"%s\" \"%s\"", luajit_path, setup, launch_path, project_path);
   
   memset(&startupInfo, 0, sizeof(startupInfo));
   memset(&processInfo, 0, sizeof(processInfo));
